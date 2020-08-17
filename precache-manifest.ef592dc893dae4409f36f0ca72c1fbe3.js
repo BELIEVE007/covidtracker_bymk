a06548989e4cb98cb67eae7b2dc99da577a22c5c
mayankaushik <48647584+mayankaushik@users.noreply.github.com>
2020-08-18 00:14:26 +0530

    Updates

---
+++ precache-manifest.ef592dc893dae4409f36f0ca72c1fbe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49767cbd3ce774e6074ccee4ec7191ce",
+    "url": "/covidtracker_bymk/index.html"
+  },
+  {
+    "revision": "8ead9de7ae51190717aa",
+    "url": "/covidtracker_bymk/static/css/main.2a491197.chunk.css"
+  },
+  {
+    "revision": "82920ca7a65c8ac343d7",
+    "url": "/covidtracker_bymk/static/js/2.6680a3c6.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covidtracker_bymk/static/js/2.6680a3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ead9de7ae51190717aa",
+    "url": "/covidtracker_bymk/static/js/main.10301666.chunk.js"
+  },
+  {
+    "revision": "7428f6da6a4d17a5933c",
+    "url": "/covidtracker_bymk/static/js/runtime-main.ec1a21d1.js"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/covidtracker_bymk/static/media/image.6ce9074d.png"
+  }
+]);
